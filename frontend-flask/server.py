e31c63d1d703a33182485bf00a86e5e4ca863052
lukshan13 <lukshan@hotmail.co.uk>
2019-03-18 10:49:24 +0000

    A couple more changes

--- frontend-flask/server.py
+++ frontend-flask/server.py
@@ -2,15 +2,17 @@ from flask import Flask, abort, render_template, redirect, make_response, url_fo
 from flask import request
 
 import re
-
+rooms = []
 
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+
 @app.route("/", methods = ["GET","POST"])
 def home():
-	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')))
+	print(listRooms())
+	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')), listRooms())
 
 	if request.method == 'GET':
 		if not userData[0] and not userData[1]:
@@ -23,7 +25,7 @@ def home():
 		if not userData[0] and not userData[1]:
 			resp = make_response(redirect(url_for('home')))
 			resp.set_cookie('username', sanitiseInput(request.form['username']))
-			resp.set_cookie('userID', generateID())
+			resp.set_cookie('userID', generateID(0xFFFFFFFF))
 			return resp
 		else:
 			pass#Do room stuff
@@ -33,7 +35,7 @@ def home():
 @app.route("/refreshID", methods = ["GET","POST"])
 def GetMeANewID():
 
-	userData = (request.cookies.get('username'), request.cookies.get('userID'))
+	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')))
 
 	if not userData[0] and not userData[1]:
 		flash("You have not provided a username yet", 'warning')
@@ -44,16 +46,24 @@ def GetMeANewID():
 	elif request.method == 'POST':
 		resp = make_response(redirect(url_for('home')))
 	resp.set_cookie('username', userData[0])
-	resp.set_cookie('userID', generateID())
+	resp.set_cookie('userID', generateID(0xFFFFFFFF))
 
 	return resp
 
 
-@app.route("/create_room")
+@app.route("/create_room", methods = ['GET', 'POST'])
 def create_room_page():
+	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')), listRooms())
+
 	if not request.remote_addr.startswith('127'):
 		abort(403)
-	return ("hi")
+
+	if request.method == 'GET':
+		return (render_template("CreateRoom.html", userData = userData))
+	elif request.method == 'POST':
+		game_room(request.form['roomName'])
+		flash("Room has been created", 'success')
+		return redirect(url_for('home'))
 
 
 def sanitiseInput(input):
@@ -65,11 +75,28 @@ def sanitiseInput(input):
 
 
 
-def generateID():
+def generateID(val):
 	import random
-	return ("{:06x}".format(random.randint(0, 0xFFFFFFFF)))
+	return ("{:06x}".format(random.randint(0, val)))
+
+
+def listRooms():
+	a = []
+	for i in rooms:
+		a.append([i.roomName, i.roomID, len(i.roomUsers), i.activeGame])
+	return a
 
 
 class game_room:
-	def __init__():
-		pass
+	def __init__(self, roomName):
+		self.roomName = roomName
+		self.roomID = generateID(0xFFFFFF)
+		self.roomUsers = []
+		self.activeGame = False
+		self.AddToActiveRooms()
+
+	def AddToActiveRooms(self):
+		rooms.append(self)
+
+	def RemoveFromActiveRooms(self):
+		rooms.remove(self)
