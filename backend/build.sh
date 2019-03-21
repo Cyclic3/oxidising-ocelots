3eedcddabb98b2bb4303a9bef54c82cf88e7485e
Cyclic3 <cyclic3.git@gmail.com>
2019-03-21 12:09:55 +0000

    Updated build script so that root is no longer required

--- backend/build.sh
+++ backend/build.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 set -e -x
 
-SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SOURCEDIR="$(dirname $0 | xargs realpath)"
 
 BASEDIR=$(mktemp -d)
 
 cd $BASEDIR
-function nu() {
+mkdir -p build
+cd build
+
+{
 	git clone https://github.com/c3-projects/nu.git
 	cd nu
 	git checkout develop
-	mkdir -p build
-	cd build
-	cmake ..
-	cmake --build .
-	cmake --build . --target test
-#	cmake --build .
-	if [ -f "/etc/debian_version" ]; then
-		cmake --build . --target package
-		sudo dpkg -i *.deb
-	else
-		cmake --build . --target install
-	fi
-	cd $BASEDIR
-	rm -rf nu
+	cd ..
 }
 
 {
-	mkdir -p build
-	cd build
-	cmake "$SOURCEDIR" -DCMAKE_BUILD_TYPE=Debug
+	cmake "$SOURCEDIR" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="$BASEDIR/build/nu"
 	cmake --build .
 	cp *oxidising-ocelots-backend* ..
 	cd $BASEDIR
