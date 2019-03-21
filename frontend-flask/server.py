6b81973382129c73e933e1be89c5d8dbf38e1b45
lukshan13 <lukshan@hotmail.co.uk>
2019-03-21 15:38:23 +0000

    updoots
    updoots. Players can only join one room at a time, implemented time out for players who have left, giving them ability to rejoin. API for backend started

--- frontend-flask/server.py
+++ frontend-flask/server.py
@@ -5,33 +5,62 @@ try:
 except:
 	while True:
 		input("You are missing dependencies. Please use pip to install the following \n Flask \n Flask-SocketIO==3.1.2")
-import re, json
+import re, json, time
 from threading import Thread
 rooms = {}
 
 
 oldList = activeList = []
-checked = False
+USER_TIMEOUT = 10
 
-def checkAlive():
-	if checked == False:
-		oldList = activeList
-		activeList = []
-		socketio.emit('AreYouAlive')
-		checked = True
-		return False
-		
-	elif checked == True:
-		
-		checked = False
-		return ("please check console")
 
+
+
+def checkAlive(sleeper):
+	time.sleep(sleeper)
+	global activeList, oldList
+	oldList = activeList
+	activeList = []
+	socketio.emit('AreYouAlive')
+
+	print ("Poll sent")
+		
+	time.sleep(sleeper)		
+
+	if len(oldList) == 0:
+		joinedUsers = activeList
+		leftUsers = []
+	elif len(activeList)  == 0:
+		leftUsers = oldList
+		joinedUsers = []
+
+	else:
+		joinedUsers = list(set(oldList)-set(activeList))
+		leftUsers = list(set(oldList)-set(activeList))
+
+	if not leftUsers == []:
+		for room in rooms:
+			for user in oldList:
+				if user in rooms[room].activeRoomUsers:
+					rooms[room].RemoveActiveUserFromRoom(user)
+
+	return('New Users:' +str(joinedUsers)+ '.  users who have left:' +str(leftUsers))
+
+
+def checkAliveCycle(debug):
+	if debug:
+		print('auto polling not active while in debug mode. Please run server with debug = False')
+		return
+	while True:
+		print (checkAlive(USER_TIMEOUT))
 
 
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 socketio = SocketIO(app)
+
+
 
 
 @app.route("/", methods = ["GET","POST"])
@@ -113,23 +142,17 @@ def room(roomRefrence):
 	if not roomRefrence in rooms:
 		flash('That room cannot be found','warning')
 		return redirect(url_for('home'))
+
 	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')), listRooms())
+
+
+	for room in rooms:
+		print (rooms[room].activeRoomUsers)
+		if userData[1] in rooms[room].activeRoomUsers:
+			flash("You appear to already be in a room. Perhaps you are in another tab? If you have just left a game, please wait upto 10 seconds before trying to connect", 'warning')
+			return redirect(url_for('home'))
 	return render_template('room.html', userData = userData)
 
-
-
-@app.route("/au")
-def au():
-	global a, activeList
-	if a == False:
-		activeList = []
-		socketio.emit('AreYouAlive')
-		a = True
-		return ('polling, please refresh page in a few seconds')
-	elif a == True:
-		print (activeList)
-		a = False
-		return ("please check console")
 
 
 @socketio.on('IAmAlive')
@@ -138,40 +161,47 @@ def amAliveNotification():
 	activeList.append(request.cookies.get('userID'))
 
 
-
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 
 @socketio.on('connect')
 def on_connect():
-
-    jsonData = {
-    'userID': sanitiseInput(request.cookies.get('userID')),
-    'user_name': sanitiseInput(request.cookies.get('username')),
-    'message': '<b><i>User Connected</i></b>'
-    }
-    socketio.emit('ActionResponse', jsonData, callback=messageReceived)
+	activeList.append(request.cookies.get('userID'))
+	jsonData = {
+	'user_name': sanitiseInput(request.cookies.get('username')),
+	'message': '<b><i>User Connected</i></b>'
+	}
+	socketio.emit('ActionResponse', jsonData, callback=messageReceived)
 
 @socketio.on('disconnect')
 def on_disconnect():
 
     jsonData = {
-    'userID': sanitiseInput(request.cookies.get('userID')),
     'user_name': sanitiseInput(request.cookies.get('username')),
     'message': '<b><i>User Disconnected</i></b>'
     }
+
+    print ('\n\nuserdisconnected\n\n')
     socketio.emit('ActionResponse', jsonData, callback=messageReceived)
 
+
+@socketio.on('registerToRoom')
+def regsiterToRoom(data, methods=['GET', 'POST']):
+	userData = (request.cookies.get('username'), sanitiseInput(request.cookies.get('userID')), listRooms())
+
+	lis = (request.sid)
+
+
+	roomRefrence = (data['roomID'])
+	currentRoom = rooms.get(roomRefrence)
+	currentRoom.activeRoomUsers[userData[1]] = (lis)
 
 
 @socketio.on('ActionHappened')
 def handle_my_custom_event(jsonData, methods=['GET', 'POST']):
-	print ("\n\n\n\n")
+
 	print (jsonData)
-
-
-	jsonData['userID'] = request.cookies.get('userID')
 	jsonData['username'] = request.cookies.get('username')
 	jsonData['user_name'] = request.cookies.get('username')
 
@@ -182,14 +212,12 @@ def handle_my_custom_event(jsonData, methods=['GET', 'POST']):
 		jsonData['message'] = ("<b><i>"+jsonData.pop('messageBold')+"</i></b>")
 
 
-	print (jsonData)
-	print ("\n\n\n\n")
 	socketio.emit('ActionResponse', jsonData, callback=messageReceived)
 
 
 
 
-#<-------------------------------------------------------------------------------->
+########################################################################################################################
 
 def sanitiseInput(input):
 	if input:
@@ -209,7 +237,7 @@ def listRooms():
 	a = []
 	for i in rooms:
 		i = (rooms[i])
-		a.append([i.roomName, i.roomID, len(i.roomUsers), i.activeGame])
+		a.append([i.roomName, i.roomID, len(i.activeRoomUsers), i.activeGame])
 	return a
 
 
@@ -217,7 +245,8 @@ class game_room:
 	def __init__(self, roomName):
 		self.roomName = roomName
 		self.roomID = generateID(0xFFFFFFFF)
-		self.roomUsers = []
+		self.activeRoomUsers = {}
+		self.allactiveRoomUsers = {}
 		self.activeGame = False
 		self.AddToActiveRooms()
 
@@ -228,8 +257,19 @@ class game_room:
 		del rooms[self.roomID]
 
 	def AddUserToRoom(self, userID):
-		self.roomUsers.append(userID)
+		self.activeRoomUsers.append(userID)
+
+	def RemoveActiveUserFromRoom(self, userID):
+		del self.activeRoomUsers[userID]
+
+
+#####
+GetRid = game_room('lukshans room')
+#####
+
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
-    #socketio.run(app, debug=True)
+	debug = False
+	Thread(target = checkAliveCycle, args =(debug,) ).start()
+	socketio.run(app, debug=debug, host='0.0.0.0')
+	#socketio.run(app, debug=True)
